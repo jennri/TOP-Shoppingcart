0cdeb6c6c2b8635a81ca7a847ce984a563a829e2
jennri <121411741+jennri@users.noreply.github.com>
2023-07-19 12:34:26 +1000

    some styling

--- src/components/footbar.jsx
+++ src/components/footbar.jsx
@@ -1,36 +1,72 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import './footbar.css';
-import { Col, Row } from 'antd';
+import { Col, Row, Space, Input, Button } from 'antd';
+import { FacebookOutlined, 
+        InstagramOutlined,
+        TwitterOutlined,
+        WechatOutlined
+        } from '@ant-design/icons'
+import './bar.css'
 
 
 export const Footbar = () => {
     return (
         <div>
             <Row>
-                <Col span={8}>
+                <Col span={8} className="foot-bar__left">
+                    <div className="foot-bar__left-text">
                     <h1>About Our Company</h1>
+                    <p>Lehmaansa lie rahasumma tassakaan lipullaan vielakaan eri. Liikkeelle osa eli paaltapain kuitenkaan ota. Ota maailman aittanne kaljaasi jaa niinkuin moottori lasianne. </p>
+
+                    
+                    </div>
+
+                    <div className="foot-bar__left-socialmedia">
+                    <p>Follow Us!</p>
+                    <Link> <FacebookOutlined /></Link>
+                    <Link> <InstagramOutlined /></Link>
+                    <Link> <TwitterOutlined /></Link>
+                    <Link> <WechatOutlined /></Link>
+                    </div>
+
                 </Col>
 
-                <Col span={8}>
-                    <ul>
-                        <li><Link to="/contact-us"> Contact us </Link></li>
-                        <li><Link to="/shipping-policy"> Shipping Policy </Link></li>
-                        <li><Link to="/privacy-policy"> Privacy Policy </Link></li>
-                        <li><Link to="/frequently-asked-questions"> FAQs </Link></li>
-                        <li><Link to="/refund-policy"> Refund Policy </Link></li>
-                        <li><Link to="/terms-of-services"> Terms of Services </Link></li>
-                    </ul>
+
+                <Col span={4}  className="foot-bar__middle-help">
+                    <h2>Quick Help</h2>
+                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                    <Link to="/contact-us"> Contact us </Link>
+                    {/* My account */}
+                    <Link to="/shipping-policy"> Shipping Policy </Link>
+                    {/* Order tracking */}
+                    {/* Customer Experience */}
+                    <Link to="/privacy-policy"> Privacy Policy </Link>
+                    <Link to="/refund-policy"> Refund Policy </Link>
+                    <Link to="/frequently-asked-questions"> FAQs </Link>
+                </Space>
                 </Col>
 
-                <Col span={8}>
+                <Col span={4}  className="foot-bar__middle-about"> 
+                    <h2>Get to know us</h2>
+                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                    <Link to="/terms-of-services"> Terms of Services </Link>
+                    {/* Careers, About Us, Why Shop here */}
+                </Space>
+                </Col>
+
+
+                <Col span={8}  className="foot-bar__right">
+                    <div className="foot-bar__right-container"> 
+                <Space direction="vertical" size="small" align="start">
                     <h1>Newsletter</h1>
                     <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
-                    <form>
-                        <label htmlFor="subemail"></label>
-                        <input id="subemail" name="subemail" placeholder="Enter your email" type="email" required />
-                        <button type="submit">Subscribe</button>
-                    </form>
+                    <Input
+                    placeholder="Email address"
+                    style={{ width: 200 }}
+                    />
+                    <Button>Grape!</Button>
+                    </Space>
+                    </div>
                 </Col>
             </Row>
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,57 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-import './navbar.css'
-import { } from 'antd'
+import './bar.css'
+import { Input, Button } from 'antd'
 import { Col, Row } from 'antd';
-import { UserOutlined, SearchOutlined, ShoppingFilled } from '@ant-design/icons'
+import { UserOutlined, 
+        SearchOutlined, 
+        ShoppingFilled, 
+        HeartOutlined,
+         } from '@ant-design/icons'
+import logo from '../assets/logo-transparent-title.png'
+    
 
 
 export const Navbar = () => {
     return (
         <nav>
             <Row>
-                <Col span={6}><p>Empty Space</p></Col>
-                <Col span={12}>
-                    <p>Logo goes here, links to home page</p>
+                <Col span={6} className='nav-bar__left'>
+                    <div className='nav-bar__left-search-container'>
+                    <Input
+                    placeholder="Whatcha lookin for?"
+                    style={{ width: 200 }}
+                    />
+                    <Button icon={<SearchOutlined />} />
+                    </div>
                 </Col>
 
-                <Col span={6}>
-                    <Link to="/search"> <SearchOutlined /> </Link>
+                <Col span={12} className='nav-bar__middle'>
+                    <img src={logo} alt='logo' />
+                </Col>
+
+                <Col span={6} className='nav-bar__right'>
+                    <div className="nav-bar__right-icon-container">
                     <Link to="/account"> <UserOutlined /> </Link>
+                    <Link><HeartOutlined /></Link>
                     <Link to="/cart"> <ShoppingFilled /> </Link>
+                    </div>
                 </Col>
             </Row>
 
             <Row>
-            <Col span={24}>
-                <Link to="/">Home</Link>
-                <Link to="/new">New</Link>
-                <Link to="/sale">Sale</Link>
+            <Col span={24} className='nav-bar__bottom'>
+                <div className="nav-bar__bottom--container">
+                <Link to="/">HOME</Link>
+                <Link to="/new">NEW</Link>
+                <Link to="/sale">SALE</Link>
                 {/* This will be a filtered page of the product page */}
-                <Link to="/blog">Blog</Link>
-                <Link to="/products">Products</Link>
+                <Link to="/products">PRODUCTS</Link>
                 {/* This will split into skincare and categories link */}
-                <Link to="/about-us">About Us</Link>
+                <Link to="/blog">BLOG</Link>
+                <Link to="/about-us">ABOUT US</Link>
+                </div>
             </Col>
             </Row>
         </nav>
