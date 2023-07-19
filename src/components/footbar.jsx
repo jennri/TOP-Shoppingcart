bf34ce247dea1184839e2273632b49ebdcd4679f
jennri <121411741+jennri@users.noreply.github.com>
2023-07-19 15:51:29 +1000

    last update before adding modals

--- src/components/footbar.jsx
+++ src/components/footbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { Col, Row, Space, Input, Button } from 'antd';
-import { FacebookOutlined, 
-        InstagramOutlined,
-        TwitterOutlined,
-        WechatOutlined
-        } from '@ant-design/icons'
+import {
+    FacebookOutlined,
+    InstagramOutlined,
+    TwitterOutlined,
+    WechatOutlined
+} from '@ant-design/icons'
 import './bar.css'
 
 
@@ -15,57 +16,61 @@ export const Footbar = () => {
             <Row>
                 <Col span={8} className="foot-bar__left">
                     <div className="foot-bar__left-text">
-                    <h1>About Our Company</h1>
-                    <p>Lehmaansa lie rahasumma tassakaan lipullaan vielakaan eri. Liikkeelle osa eli paaltapain kuitenkaan ota. Ota maailman aittanne kaljaasi jaa niinkuin moottori lasianne. </p>
+                        <h1>About Our Company</h1>
+                        <p>Lehmaansa lie rahasumma tassakaan lipullaan vielakaan eri. Liikkeelle osa eli paaltapain kuitenkaan ota. Ota maailman aittanne kaljaasi jaa niinkuin moottori lasianne. </p>
 
-                    
+
                     </div>
 
                     <div className="foot-bar__left-socialmedia">
-                    <p>Follow Us!</p>
-                    <Link> <FacebookOutlined /></Link>
-                    <Link> <InstagramOutlined /></Link>
-                    <Link> <TwitterOutlined /></Link>
-                    <Link> <WechatOutlined /></Link>
+                        <p>Follow Us!</p>
+                        <Link> <FacebookOutlined /></Link>
+                        <Link> <InstagramOutlined /></Link>
+                        <Link> <TwitterOutlined /></Link>
+                        <Link> <WechatOutlined /></Link>
                     </div>
 
                 </Col>
 
 
-                <Col span={4}  className="foot-bar__middle-help">
+                <Col span={4} className="foot-bar__middle-help">
                     <h2>Quick Help</h2>
-                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                    <Link to="/contact-us"> Contact us </Link>
-                    {/* My account */}
-                    <Link to="/shipping-policy"> Shipping Policy </Link>
-                    {/* Order tracking */}
-                    {/* Customer Experience */}
-                    <Link to="/privacy-policy"> Privacy Policy </Link>
-                    <Link to="/refund-policy"> Refund Policy </Link>
-                    <Link to="/frequently-asked-questions"> FAQs </Link>
-                </Space>
-                </Col>
-
-                <Col span={4}  className="foot-bar__middle-about"> 
-                    <h2>Get to know us</h2>
-                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                    <Link to="/terms-of-services"> Terms of Services </Link>
-                    {/* Careers, About Us, Why Shop here */}
-                </Space>
-                </Col>
-
-
-                <Col span={8}  className="foot-bar__right">
-                    <div className="foot-bar__right-container"> 
-                <Space direction="vertical" size="small" align="start">
-                    <h1>Newsletter</h1>
-                    <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
-                    <Input
-                    placeholder="Email address"
-                    style={{ width: 200 }}
-                    />
-                    <Button>Grape!</Button>
+                    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                        <Link to="/contact-us"> Contact us </Link>
+                        {/* My account */}
+                        <Link to="/shipping-policy"> Shipping Policy </Link>
+                        {/* Order tracking */}
+                        {/* Customer Experience */}
+                        <Link to="/privacy-policy"> Privacy Policy </Link>
+                        <Link to="/refund-policy"> Refund Policy </Link>
+                        <Link to="/frequently-asked-questions"> FAQs </Link>
                     </Space>
+                </Col>
+
+                <Col span={4} className="foot-bar__middle-about">
+                    <h2>Get to know us</h2>
+                    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                        <Link to="/terms-of-services"> Terms of Services </Link>
+                        {/* Careers, About Us, Why Shop here */}
+                    </Space>
+                </Col>
+
+
+                <Col span={8} className="foot-bar__right">
+                    <div className="foot-bar__right-container">
+                        <Space direction="vertical" size="medium" align="start">
+                            <div>
+                                <Space direction="vertical" size="medium" align="start">
+                                    <h1>Newsletter</h1>
+                                    <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
+                                </Space>
+                            </div>
+                            <Input
+                                placeholder="Email address"
+                                style={{ width: 200 }}
+                            />
+                            <Button>Grape!</Button>
+                        </Space>
                     </div>
                 </Col>
             </Row>
