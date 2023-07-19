df2febc5d2fffd93d76551a5690f2f56feadd924
jennri <121411741+jennri@users.noreply.github.com>
2023-07-20 00:18:22 +1000

    modal scroll works

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import { Cart } from '../pages/shop/cart'
 
 import './bar.css'
 import '../pages/shop/cart-modal.css'
 
-import { Input, Button, Modal } from 'antd'
+import Modal from 'react-modal';
+import { Input, Button } from 'antd'
 import { Col, Row } from 'antd';
-import { UserOutlined, 
-        SearchOutlined, 
-        ShoppingFilled, 
-        HeartOutlined,
-         } from '@ant-design/icons'
+import {
+    UserOutlined,
+    SearchOutlined,
+    ShoppingFilled,
+    HeartOutlined,
+} from '@ant-design/icons'
 import logo from '../assets/logo-transparent-title.png'
-    
 
 export const Navbar = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-      setIsModalOpen(true);
-    };
-    const handleOk = () => {
-      setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-      setIsModalOpen(false);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+        document.body.style.overflow = 'hidden';
+
+    }
+    function closeModal() {
+        document.body.style.overflow = 'scroll'
+        setIsOpen(false);
+    }
+
+    const modalStyle = {
+        content: {
+        position: "absolute",
+        borderRadius: ".5em",
+        left: '70%',
+        MarginRight: '-70%',
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        }
+
     };
 
     return (
@@ -32,11 +46,11 @@ export const Navbar = () => {
             <Row>
                 <Col span={6} className='nav-bar__left'>
                     <div className='nav-bar__left-search-container'>
-                    <Input
-                    placeholder="Whatcha lookin for?"
-                    style={{ width: 200 }}
-                    />
-                    <Button icon={<SearchOutlined />} />
+                        <Input
+                            placeholder="Whatcha lookin for?"
+                            style={{ width: 200 }}
+                        />
+                        <Button icon={<SearchOutlined />} />
                     </div>
                 </Col>
 
@@ -46,40 +60,36 @@ export const Navbar = () => {
 
                 <Col span={6} className='nav-bar__right'>
                     <div className="nav-bar__right-icon-container">
-                    <Link to="/account"> <UserOutlined /> </Link>
-                    <Link><HeartOutlined /></Link>
-                    <Link onClick={showModal}> <ShoppingFilled /> </Link>
-                    <Modal className='modal-test' open={isModalOpen} onOk={handleOk} onCancel={handleCancel} 
-                    okText="Checkout"
-                    cancelText="Back"
-                    okButtonProps={{
-                       
-                      }}
-                    cancelButtonProps
-                 
-                    >
-                        <Cart />
-                    </Modal>
+                        <Link to="/account"> <UserOutlined /> </Link>
+                        <Link><HeartOutlined /></Link>
+                        <Link onClick={openModal}> <ShoppingFilled /> </Link>
+
+                        <Modal
+                            isOpen={modalIsOpen}
+                            onRequestClose={closeModal}
+                            style={modalStyle}>
+                            <button onClick={closeModal}>x</button>
+                            <Cart />
+                        </Modal>
+
                     </div>
                 </Col>
             </Row>
 
             <Row>
-            <Col span={24} className='nav-bar__bottom'>
-                <div className="nav-bar__bottom--container">
-                <Link to="/">HOME</Link>
-                <Link to="/new">NEW</Link>
-                <Link to="/sale">SALE</Link>
-                {/* This will be a filtered page of the product page */}
-                <Link to="/products">PRODUCTS</Link>
-                {/* This will split into skincare and categories link */}
-                <Link to="/blog">BLOG</Link>
-                <Link to="/about-us">ABOUT US</Link>
-                </div>
-            </Col>
+                <Col span={24} className='nav-bar__bottom'>
+                    <div className="nav-bar__bottom--container">
+                        <Link to="/">HOME</Link>
+                        <Link to="/new">NEW</Link>
+                        <Link to="/sale">SALE</Link>
+                        {/* This will be a filtered page of the product page */}
+                        <Link to="/products">PRODUCTS</Link>
+                        {/* This will split into skincare and categories link */}
+                        <Link to="/blog">BLOG</Link>
+                        <Link to="/about-us">ABOUT US</Link>
+                    </div>
+                </Col>
             </Row>
         </nav>
     )
 }
-
-export default Navbar;
