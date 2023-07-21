d91ccd3673f551c8f898c71cda6a034bf05c6dcb
jennri <121411741+jennri@users.noreply.github.com>
2023-07-21 15:14:31 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,16 +10,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // To connect with your mongoDB database
-// const dbURI = 'mongodb+srv://admin:1234eight@beautybubble.5t61dmy.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect(dbURI)
-// .then((result) =>  app.listen(3000))
-// .catch((err) => console.log(err))
+const dbURI = 'mongodb+srv://admin:1234eight@beautybubble.5t61dmy.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+.then((result) =>  app.listen(8000))
+.catch((err) => console.log(err))
 
     
 app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "hehe" });
 });
 
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
-});
