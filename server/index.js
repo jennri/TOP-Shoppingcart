0ebb0b3d94f0bb019376e02b6741ffeb0cf155f4
jennri <121411741+jennri@users.noreply.github.com>
2023-07-24 16:06:27 +1000

    small change

--- server/index.js
+++ server/index.js
@@ -12,10 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 // To connect with your mongoDB database
 const dbURI = 'mongodb+srv://admin:1234eight@beautybubble.5t61dmy.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
-.then((result) =>  app.listen(8000))
+.then((result) =>  app.listen(8000, () => {
+    console.log('Server is listenning on port 8000')
+}))
 .catch((err) => console.log(err))
 
-    
 app.get('/message', (req, res) => {
     res.json({ message: "Hello from the backend" });
 });
